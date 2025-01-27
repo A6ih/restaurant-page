3672bb293b9588f3c52f6b0a202cd50f41bfbbf0
A6ih <177154114+A6ih@users.noreply.github.com>
2025-01-27 23:49:49 +0200

    Add switchMainContent() to switch between tab buttons and main body content

--- src/index.js
+++ src/index.js
@@ -2,4 +2,36 @@ import "./styles.css"
 import createHomePage from "./home"
 
 const bodyContent = document.querySelector("#content");
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const contactBtn = document.querySelector("#contact-btn");
+const buttons = [homeBtn, menuBtn, contactBtn];
+buttons.forEach(buttons => buttons.addEventListener("click", switchMainContent));
+
+function switchMainContent(event) {
+    const target = event.target.id;
+    switch(target){
+        case "home-btn":
+            menuBtn.style.animation = "none";
+            contactBtn.style.animation = "none";
+            homeBtn.style.animation = "flicker 1.5s infinite alternate";
+            bodyContent.textContent = "";
+            bodyContent.appendChild(createHomePage());
+        break;
+        case "menu-btn":
+            contactBtn.style.animation = "none";
+            homeBtn.style.animation = "none";
+            menuBtn.style.animation = "flicker 1.5s infinite alternate";
+            bodyContent.textContent = "";
+        break;
+        case "contact-btn":
+            menuBtn.style.animation = "none";
+            homeBtn.style.animation = "none";
+            contactBtn.style.animation = "flicker 1.5s infinite alternate";
+            bodyContent.textContent = "";
+        break;
+    }
+}
+
+homeBtn.style.animation = "flicker 1.5s infinite alternate";
 bodyContent.appendChild(createHomePage());
